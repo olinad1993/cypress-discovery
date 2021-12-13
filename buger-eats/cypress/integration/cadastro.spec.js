14a94ec8d1ba57052a50fd3479d92bec4e29f88b
olinad1993 <olinad1993@gmail.com>
2021-12-13 16:31:35 -0300

    Automatizando pagina de cadastro

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -5,6 +5,8 @@ describe('Cadastro' , () => {
         cy.visit('https://buger-eats.vercel.app')
 
         cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') // problema no espaço da linha
+
 
     })
 })
