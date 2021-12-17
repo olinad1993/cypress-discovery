2f9a90e2893111ee5ab5e8b8e24f1be5f031b5df
olinad1993 <olinad1993@gmail.com>
2021-12-17 12:26:55 -0300

    Automatizando cadastro do entregador

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,4 +1,4 @@
-describe('Cadastro' , () => {
+describe('Cadastro de Entregador' , () => {
 
     it('Usuário deve se tornar um entregador' , () => {
         cy.viewport(1440,900)
@@ -7,6 +7,33 @@ describe('Cadastro' , () => {
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas') // problema no espaço da linha
 
+        var entregador = {
+            nome:'Danilo Ferreira',
+            cpf: '00000014141',
+            email: 'danilo@hotmail.com',
+            whatsapp: '22999999999',
 
+            endereco: {
+                cep:'28470000',
+                rua: 'Rua dos Bobos',
+                numero: '100',
+                complemento: 'Ap 102',
+                bairro: 'Casa Velha',
+                cidade_uf: 'Pádua/RJ'
+            }
+
+        }
+
+        cy.get('input[name="name"]').type(entregador.nome);
+        cy.get('input[name="cpf"]').type(entregador.cpf);
+        cy.get('input[name="email"]').type(entregador.email);
+        cy.get('input[name="whatsapp"]').type(entregador.whatsapp);
+
+        // ENDEREÇO
+
+        cy.get('input[name="postalcode"]').type(entregador.endereco.cep);
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+        cy.get('input[name="address-number"]').type(entregador.endereco.numero);
+        cy.get('input[name="address-details"]').type(entregador.endereco.complemento);
     })
 })
