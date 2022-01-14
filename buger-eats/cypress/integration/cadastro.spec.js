d13123137697e79834a8834b4019efd0086faa10
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-01-13 22:45:22 -0300

    Identando codigos

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -2,18 +2,17 @@ import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro de Entregador' , () => {
     
-    beforeEach(()=>{
-        cy.fixture('deliver').then((d)=>{
+    beforeEach(()=> {
+        cy.fixture('deliver').then((d)=> {
             this.deliver = d
         })
     })
 
-    let signup = new SignupPage()
+     let signup = new SignupPage()
 
     it('Usuário deve se tornar um entregador' , () => {
-
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.signup)
         signup.submit()
 
         const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
@@ -23,7 +22,7 @@ describe('Cadastro de Entregador' , () => {
     it('CPF incorreto' , () => {
 
         signup.go()
-        signup.fillform(deliver)
+        signup.fillform(this.deliver.cpf_inv)
         signup.submit()
         signup.alertMessageShouldBe('Oops! CPF inválido')
     })
