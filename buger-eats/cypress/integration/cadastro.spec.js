c59291c234265eae714a59e50944148fc6623162
Danilo Ferreira <85970991+olinad1993@users.noreply.github.com>
2022-01-13 22:29:53 -0300

    Deliver no json

--- buger-eats/cypress/integration/cadastro.spec.js
+++ buger-eats/cypress/integration/cadastro.spec.js
@@ -1,46 +1,16 @@
 import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro de Entregador' , () => {
+    
+    beforeEach(()=>{
+        cy.fixture('deliver').then((d)=>{
+            this.deliver = d
+        })
+    })
 
     let signup = new SignupPage()
 
-    // before(function (){
-    //     cy.log('Tudo aqui é executado uma unica vez ANTES de TODOS os casos de testes')
-    // })
-
-    // beforeEach(function (){
-    //     cy.log('Tudo aqui é executado sempre ANTES de CADA caso de teste')
-    // })
-
-
-    // after(function (){
-    //     cy.log('Tudo aqui é executado uma unica vez DEPOIS de TODOS os casos de testes')
-    // })
-
-    // afterEach(function(){
-    //     cy.log('Tudo aqui é executado uma unica vez DEPOIS de TODOS os casos de testes')
-    // })
-
     it('Usuário deve se tornar um entregador' , () => {
-
-        let deliver = {
-            name:'Danilo Ferreira',
-            cpf: '00000014141',
-            email: 'danilo@hotmail.com',
-            whatsapp: '22999999999',
-
-            address: {
-                postalcode:'04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '100',
-                details: 'Ap 102',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
 
         signup.go()
         signup.fillform(deliver)
@@ -51,25 +21,6 @@ describe('Cadastro de Entregador' , () => {
     })
 
     it('CPF incorreto' , () => {
-
-        let deliver = {
-            name:'Danilo Ferreira',
-            cpf: '000000141AA',
-            email: 'danilo@hotmail.com',
-            whatsapp: '22999999999',
-
-            address: {
-                postalcode:'04534011',
-                street: 'Rua Joaquim Floriano',
-                number: '100',
-                details: 'Ap 102',
-                district: 'Itaim Bibi',
-                city_state: 'São Paulo/SP'
-            },
-
-            delivery_method: 'Moto',
-            cnh: 'cnh-digital.jpg'
-        }
 
         signup.go()
         signup.fillform(deliver)
